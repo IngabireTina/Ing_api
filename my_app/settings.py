220ffa195ee53902cf96bfec3bc4b38325c3ff10
IngabireTina <ingabire067@gmail.com>
2021-04-26 06:16:23 -0400

    added database

--- my_app/settings.py
+++ my_app/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'my_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rating',
+        'USER': 'test',
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        }
+
     }
-}
 
 
 # Password validation
