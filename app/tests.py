ec8adb6393c6f9d2c16af15fea574cb46d867dc7
IngabireTina <ingabire067@gmail.com>
2021-04-26 10:44:30 -0400

    added tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Profile,Projects,Rates
+
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.wanjiku = Profile( profile_photo='default.jpg',bio='hello you', website='www.tina.com', phone_number='078656443')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.wanjiku,Profile))
+
+    def test_save(self):
+        self.wanjiku.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+ 
+
+ class PostsTestClass(TestCase):
+    def setUp(self):
+        self.wanjiku = Profile(first_name = 'Wanjiku',last_name='Kariuki',username='ciku_k',email='sheekokariuki@gmail.com')
+        self.wanjiku.save_profile()
+
+        self.new_tag=tag(tag='testing')
+        self.new_tag.save()
+
+        self.new_post =Posts(caption="testing testing 1,2",profile=self.wanjiku)
+        self.new_post.save()
+
+        self.new_post.tag.add(self.new_tag)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        tag.objects.all().delete()
+        Posts.objects.all().delete()    
+
+    def test_posts(self):
+        posts = Posts.posts()
+        self.assertTrue(len(posts)>0)
+
+
+class RatesTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username='ketty',email='ketty@gmail.com',password='tina12345')
+        
+        self.rate = Rates(design=10,usability=10,content=10,user=self.user,project=10)
+        self.rate.save()
+        
+        self.assertTrue(isinstance(self.rate,Rate))        
